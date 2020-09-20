2ab74a4dcff5ea21c884013220af9938a829ded0
Nino <65510604+Nino-fr@users.noreply.github.com>
2020-09-20 10:46:47 +0200

    Add files via upload

--- .gitignore/Commands/categories.js
+++ .gitignore/Commands/categories.js
@@ -20,7 +20,7 @@ class Categories extends Command {
    * @param {Message} message
    */
   async run(message) {
-    return message.repondre(wiki.categories);
+    return message.repondre(wiki.categories.embed);
   }
 }
 
