58c590d08dab7eac8b397a1a6948ba030c1bf921
Nino <65510604+Nino-fr@users.noreply.github.com>
2020-09-30 22:41:37 +0200

    Update index.js

--- .gitignore/instagram-user/index.js
+++ .gitignore/instagram-user/index.js
@@ -11,12 +11,7 @@ module.exports = async (username) => {
 
   try {
     const url = `https://instagram.com/${username}`;
-    console.log(await got(url));
-    // console.log(await got(url, { searchParams: { __a: 1 } }));
-    /* console.log(
-      '-------------------------------------------------------\n-------------------------------------------------------------------------------------------'
-    ); */
-    // console.log(await got(url, { searchParams: { __a: 2 } }));
+
     const {
       graphql: { user },
     } = await got(url, { searchParams: { __a: 1 } }).json();
